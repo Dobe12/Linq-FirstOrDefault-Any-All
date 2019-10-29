343719569e07b91191f643927ccbd657041b2673
Andrey <cheyzercheyzer@yandex.ru>
2019-10-29 12:54:50 +0300

    fist commit

---
+++ LINQextensions/LINQextensions/EnumerableExtension.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace LINQextensions
+{
+    public static class EnumerableExtension
+    {
+        public static TSource FirstOrDefaultMy<TSource>(this IEnumerable<TSource> source)
+        {
+            IEnumerator enumerator = source.GetEnumerator();
+
+            return (enumerator.MoveNext()) ? (TSource)enumerator.Current : default(TSource);
+        }
+
+        public static TSource FirstOrDefaultMy<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+           foreach(TSource el in source)
+            {
+                if (predicate(el))
+                    return el;
+            }
+
+            return default(TSource);
+        }
+
+        public static bool AnyMy<TSourse>(this IEnumerable<TSourse> source)
+        {
+            IEnumerator enumerator = source.GetEnumerator();
+          
+            return (enumerator.MoveNext()) ? true : false;
+        }
+
+        public static bool AnyMy<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            foreach (TSource el in source)
+            {
+                if (predicate(el))
+                    return true;
+            }
+
+            return false;
+        }
+
+
+        public static bool AllMy<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            foreach (TSource el in source)
+            {
+                if (!predicate(el))
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LINQextensions/LINQextensions/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQextensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FitstorDeffault
+            //Any
+            //All
+            string[] cars = { "Alfa Romeo", "Aston Martin", "Audi", "Nissan", "Chevrolet",  "Chrysler", "Dodge", "BMW",
+                            "Ferrari",  "Bentley", "Ford", "Lexus", "Mercedes", "Toyota", "Volvo", "Subaru", "Жигули :)"};
+            string wefwe = "wefwef";
+
+            Console.WriteLine("Linq:");
+
+            Console.WriteLine(cars.FirstOrDefault());
+            Console.WriteLine(cars.FirstOrDefault(c => c.Contains("Ж")));
+            TrueOrFalsePrint(cars.Any());
+            TrueOrFalsePrint(cars.Any(c => c.Contains("Audi")));
+            TrueOrFalsePrint(cars.Any(c => c.Contains("33333333333")));
+            TrueOrFalsePrint(cars.All(c => c.Contains("Toyota")));
+            TrueOrFalsePrint(cars.All(c => char.IsUpper(c[0])));
+
+            Console.WriteLine("\nMyLinq:");
+
+            Console.WriteLine(cars.FirstOrDefaultMy());
+            Console.WriteLine(cars.FirstOrDefaultMy(c => c.Contains("Ж")));
+            TrueOrFalsePrint(cars.AnyMy());
+            TrueOrFalsePrint(cars.AnyMy(c => c.Contains("Audi")));
+            TrueOrFalsePrint(cars.AnyMy(c => c.Contains("33333333333")));
+            TrueOrFalsePrint(cars.AllMy(c => c.Contains("Toyota")));
+            TrueOrFalsePrint(cars.AllMy(c => char.IsUpper(c[0])));
+            Console.Read();
+        }
+
+        public static void TrueOrFalsePrint(bool trueOrFalse)
+        {
+            if (trueOrFalse)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+           
+    }
+}
